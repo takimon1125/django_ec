0431ff2ee51af15218aaf4a0eb1f96ea0768d6f0
takimon <takimon@sample.com>
2025-07-08 14:08:00 +0000

    商品詳細の説明文のカラムを追加

--- items/models.py
+++ items/models.py
@@ -8,6 +8,7 @@ class Items(models.Model):
     title = models.CharField(verbose_name="タイトル", max_length=255, blank=True)
     price = models.IntegerField(verbose_name="価格", blank=True)
     image = models.ImageField(verbose_name="商品画像", blank=True)
+    description = models.ImageField(verbose_name="商品説明", max_length=1000, blank=True)
 
     def __str__(self):
         return self.title

84ca3397ace77db7629b22d1707a8114dbe5d362
takimon <takimon@sample.com>
2025-07-08 13:32:53 +0000

    商品詳細画面を追加

--- items/urls.py
+++ items/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from items import views
 
 urlpatterns = [
-    path('', views.ItemsListView.as_view()),
+    path('', views.ItemsListView.as_view(), name = "items_list"),
+    path('<int:pk>/', views.ItemsDetailView.as_view(), name = "items_detail"),
 ]
--- items/views.py
+++ items/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Items
 
 # Create your views here.
 class ItemsListView(ListView):
-    template_name = "items/items_list"
     model = Items
+
+class ItemsDetailView(DetailView):
+    model = Items
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["object_list"] = Items.objects.all().order_by("-id")[0:4]
+        return context

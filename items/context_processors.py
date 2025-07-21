6e877f8473f92fe08c42f9e240d160a1a8665931
takimon <takimon@sample.com>
2025-07-21 01:40:39 +0000

    カートの個数を取得する処理をlenで個数を計算していたのmodelのcount()で計算するように修正

--- items/context_processors.py
+++ items/context_processors.py
@@ -1,5 +1,5 @@
 from .models import CartItems
 
 def cart_count(request):
-    cart_count = len(CartItems.objects.filter(cart=request.session["cart_id"])) if "cart_id" in request.session else 0
+    cart_count = CartItems.objects.filter(cart=request.session["cart_id"]).count() if "cart_id" in request.session else 0
     return {'cart_count': cart_count}
